194f4be5455b366ab7df3eaf0e762c1f8a38c956
Ilia Bozhinov <ammen99@gmail.com>
2018-06-04 14:07:22 +0300

    implement wf_duration

---
+++ src/animation.cpp
@@ -0,0 +1,52 @@
+#include "animation.hpp"
+#include <cmath>
+
+namespace wf_animation
+{
+    smooth_function linear = [] (double x) -> double { return x; };
+    smooth_function circle = [] (double x) -> double { return std::sqrt(2 * x - x * x); };
+};
+
+wf_duration::wf_duration(wf_option dur, wf_animation::smooth_function smooth)
+    : smooth_function(smooth), duration(dur) {}
+
+void wf_duration::start(double x, double y)
+{
+    begin = std::chrono::system_clock::now();
+    msec = duration->as_int();
+
+    start_value = x;
+    end_value = y;
+}
+
+double wf_duration::progress_percentage()
+{
+    using namespace std::chrono;
+    auto now = system_clock::now();
+    auto duration = duration_cast<milliseconds> (now - begin);
+
+    double p = 1. * duration.count() / msec;
+    return smooth_function(std::min(p, 1.0));
+}
+
+double wf_duration::progress()
+{
+    auto p = progress_percentage();
+    return start_value * (1 - p) + end_value * p;
+}
+
+double wf_duration::progress(double x, double y)
+{
+    auto p = progress_percentage();
+    return x * (1 - p) + y * p;
+}
+
+double wf_duration::progress(const wf_transition& transition)
+{
+    return progress(transition.start, transition.end);
+}
+
+bool wf_duration::running()
+{
+    return progress_percentage() <= 0.99;
+}
---
+++ src/animation.hpp
@@ -0,0 +1,41 @@
+#include "config.hpp"
+#include <chrono>
+
+/* Useful structures for "linear" animations,
+ * i.e animations that depend on the linear change of a quantity */
+namespace wf_animation
+{
+    using smooth_function = std::function<double(double)>;
+
+    /* TODO: maybe add sine, bounce and others? */
+    extern smooth_function linear, circle;
+}
+
+struct wf_transition
+{
+    double start, end;
+};
+
+struct wf_duration
+{
+    protected:
+        decltype(std::chrono::system_clock::now()) begin;
+        wf_animation::smooth_function smooth_function;
+        wf_option duration;
+        uint32_t msec;
+
+    public:
+        wf_duration(wf_option option,
+                    wf_animation::smooth_function smooth = wf_animation::circle);
+
+        double start_value, end_value;
+        void start(double x = 0, double y = 0);
+
+        double progress_percentage();
+
+        double progress();
+        double progress(double x, double y);
+        double progress(const wf_transition& transition);
+
+        bool running();
+};
