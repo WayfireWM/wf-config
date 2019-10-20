fbdfd59177d1a07a47b08a63da6da650b05313e6
Ilia Bozhinov <ammen99@gmail.com>
2019-10-20 19:27:29 +0200

    implement wf::config::section_t

---
+++ src/section.cpp
@@ -0,0 +1,66 @@
+#include <config/section.hpp>
+#include <map>
+
+struct wf::config::section_t::impl
+{
+  public:
+    std::map<std::string, std::shared_ptr<option_base_t>> options;
+    std::string name;
+};
+
+wf::config::section_t::section_t(const std::string& name)
+{
+    this->priv = std::make_unique<impl>();
+    this->priv->name = name;
+}
+wf::config::section_t::~section_t() = default;
+
+std::string wf::config::section_t::get_name() const
+{
+    return this->priv->name;
+}
+
+std::shared_ptr<wf::config::option_base_t>
+wf::config::section_t::get_option_or(const std::string& name)
+{
+    if (this->priv->options.count(name))
+        return this->priv->options[name];
+
+    return nullptr;
+}
+
+std::shared_ptr<wf::config::option_base_t>
+wf::config::section_t::get_option(const std::string& name)
+{
+    auto option = get_option_or(name);
+    if (!option)
+    {
+        throw std::invalid_argument("Non-existing option " + name
+            + " in config section " + this->get_name());
+    }
+
+    return option;
+}
+
+wf::config::section_t::option_list_t
+wf::config::section_t::get_registered_options() const
+{
+    option_list_t list;
+    for (auto& option : priv->options)
+        list.push_back(option.second);
+
+    return list;
+}
+
+void wf::config::section_t::register_new_option(
+    std::shared_ptr<option_base_t> option)
+{
+    if (!option)
+    {
+        throw std::invalid_argument(
+            "Cannot add null option to section " + this->get_name());
+    }
+
+    this->priv->options[option->get_name()] = option;
+}
+
---
+++ test/section_test.cpp
@@ -0,0 +1,36 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include <config/section.hpp>
+#include <config/types.hpp>
+
+TEST_CASE("wf::config::section_t")
+{
+    using namespace wf;
+    using namespace wf::config;
+
+    section_t section{"Test_Section-12.34"};
+    CHECK(section.get_name() == "Test_Section-12.34");
+    CHECK(section.get_registered_options().empty());
+    CHECK(section.get_option_or("non_existing") == nullptr);
+
+    auto intopt = std::make_shared<option_t<int_wrapper_t>>("IntOption", 123);
+    section.register_new_option(intopt);
+
+    CHECK(section.get_option("IntOption") == intopt);
+    CHECK(section.get_option_or("IntOption") == intopt);
+    CHECK(section.get_option_or("DoubleOption") == nullptr);
+
+    auto reg_opts = section.get_registered_options();
+    REQUIRE(reg_opts.size() == 1);
+    CHECK(reg_opts.back() == intopt);
+
+    auto intopt2 = std::make_shared<option_t<int_wrapper_t>>("IntOption", 125);
+    section.register_new_option(intopt2); // overwrite
+    CHECK(section.get_option_or("IntOption") == intopt2);
+
+    reg_opts = section.get_registered_options();
+    REQUIRE(reg_opts.size() == 1);
+    CHECK(reg_opts.back() == intopt2);
+}
+
