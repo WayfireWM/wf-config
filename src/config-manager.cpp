8e00f1d3b8f163c3e570d63849142ad426988298
Ilia Bozhinov <ammen99@gmail.com>
2019-10-24 12:21:57 +0200

    api: implement config-manager class to hold multiple sections

---
+++ src/config-manager.cpp
@@ -0,0 +1,70 @@
+#include <wayfire/config/config-manager.hpp>
+#include <cassert>
+#include <map>
+
+struct wf::config::config_manager_t::impl
+{
+    std::map<std::string, std::shared_ptr<section_t>> sections;
+};
+
+void wf::config::config_manager_t::merge_section(
+    std::shared_ptr<section_t> section)
+{
+    assert(section);
+    if (this->priv->sections.count(section->get_name()) == 0)
+    {
+        /* Did not exist previously, just add the new section */
+        this->priv->sections[section->get_name()] = section;
+        return;
+    }
+
+    /* Merge with existing config section */
+    auto existing_section = get_section(section->get_name());
+    auto merging_options = section->get_registered_options();
+    for (auto& option : merging_options)
+    {
+        auto existing_option =
+            existing_section->get_option_or(option->get_name());
+
+        if (existing_option) {
+            existing_option->set_value(option->get_value_str());
+        } else {
+            existing_section->register_new_option(option);
+        }
+    }
+}
+
+std::shared_ptr<wf::config::section_t>
+    wf::config::config_manager_t::get_section(const std::string& name) const
+{
+    if (this->priv->sections.count(name))
+        return this->priv->sections.at(name);
+
+    return nullptr;
+}
+
+std::shared_ptr<wf::config::option_base_t>
+    wf::config::config_manager_t::get_option(const std::string& name) const
+{
+    size_t splitter = name.find_first_of("/");
+    if (splitter == std::string::npos)
+        return nullptr;
+
+    auto section_name = name.substr(0, splitter);
+    auto option_name = name.substr(splitter + 1);
+    if (section_name.empty() || option_name.empty())
+        return nullptr;
+
+    auto section_ptr = get_section(section_name);
+    if (section_ptr)
+        return section_ptr->get_option_or(option_name);
+
+    return nullptr;
+}
+
+wf::config::config_manager_t::config_manager_t()
+{
+    this->priv = std::make_unique<impl>();
+}
+
+wf::config::config_manager_t::~config_manager_t() = default;
---
+++ test/config_manager_test.cpp
@@ -0,0 +1,70 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include <wayfire/config/config-manager.hpp>
+#include <wayfire/config/types.hpp>
+
+TEST_CASE("wf::config::config_manager_t")
+{
+    using namespace wf;
+    using namespace wf::config;
+
+    config_manager_t config{};
+
+    CHECK(config.get_option("no_such_option") == nullptr);
+    CHECK(config.get_option("section/nonexistent") == nullptr);
+    CHECK(config.get_option<int_wrapper_t>("section/nonexist/ent") == nullptr);
+
+    CHECK(config.get_section("FirstSection") == nullptr);
+    config.merge_section(std::make_shared<section_t> ("FirstSection"));
+
+    auto section = config.get_section("FirstSection");
+    REQUIRE(section != nullptr);
+    CHECK(section->get_name() == "FirstSection");
+    CHECK(section->get_registered_options().empty());
+
+    CHECK(config.get_option("FirstSection/FirstOption") == nullptr);
+
+    auto color = color_t::from_string("#FFFF").value();
+    auto option = std::make_shared<option_t<color_t>> ("ColorOption", color);
+    section->register_new_option(option);
+
+    CHECK(config.get_option<color_t>("FirstSection/ColorOption")->get_value() == color);
+    CHECK(config.get_option("FirstSection/ColorOption") == option);
+
+    auto section2 = config.get_section("SecondSection");
+    CHECK(section2 == nullptr);
+
+    auto section_overwrite = std::make_shared<section_t> ("FirstSection");
+    section_overwrite->register_new_option(
+        std::make_shared<option_t<color_t>>(
+            "ColorOption", color_t::from_string("#CCCC").value()));
+    section_overwrite->register_new_option(
+        std::make_shared<option_t<int_wrapper_t>> ("IntOption", 5));
+
+    section2 = std::make_shared<section_t> ("SecondSection");
+    section2->register_new_option(
+        std::make_shared<option_t<int_wrapper_t>> ("IntOption", 6));
+
+    config.merge_section(section_overwrite);
+    CHECK(config.get_section("FirstSection") == section); // do not overwrite
+
+    auto stored_color_opt =
+        config.get_option<color_t>("FirstSection/ColorOption");
+    REQUIRE(stored_color_opt != nullptr);
+    CHECK(stored_color_opt->get_value_str() == "#CCCCCCCC");
+
+    auto stored_int_opt =
+        config.get_option<int_wrapper_t>("FirstSection/IntOption");
+    REQUIRE(stored_int_opt);
+    CHECK(stored_int_opt->get_value_str() == "5");
+
+    config.merge_section(section2);
+    stored_int_opt = config.get_option<int_wrapper_t>("FirstSection/IntOption");
+    REQUIRE(stored_int_opt);
+    CHECK(stored_int_opt->get_value_str() == "5"); // remains same
+
+    stored_int_opt = config.get_option<int_wrapper_t>("SecondSection/IntOption");
+    REQUIRE(stored_int_opt);
+    CHECK(stored_int_opt->get_value_str() == "6");
+}
