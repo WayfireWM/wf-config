596f107ee9370ab30ca0bb961933d1e7abfa0d83
Ilia Bozhinov <ammen99@gmail.com>
2019-12-10 12:18:14 +0100

    implement wf::animation::duration_t

---
+++ src/duration.cpp
@@ -0,0 +1,96 @@
+#include <wayfire/util/duration.hpp>
+#include <chrono>
+#include <cmath>
+
+namespace wf
+{
+namespace animation
+{
+namespace smoothing
+{
+smooth_function linear  =
+    [] (double x) -> double { return x; };
+smooth_function circle  =
+    [] (double x) -> double { return std::sqrt(2 * x - x * x); };
+
+const double sigmoid_max = 1 + std::exp(-6);
+smooth_function sigmoid =
+    [] (double x) -> double { return sigmoid_max / (1 + exp(-12 * x + 6)); };
+}
+}
+}
+
+struct wf::animation::duration_t::impl
+{
+    decltype(std::chrono::system_clock::now()) start_point;
+
+    std::shared_ptr<wf::config::option_t<int>> length;
+    smoothing::smooth_function smooth_function;
+    bool is_running = false;
+
+    long get_elapsed() const
+    {
+        using namespace std::chrono;
+        auto now = system_clock::now();
+        return duration_cast<milliseconds>(now - start_point).count();
+    }
+
+    bool is_ready() const
+    {
+        int msec = std::max(1, length->get_value());
+        return get_elapsed() >= msec;
+    }
+
+    double get_progress_percentage() const
+    {
+        if (!length || is_ready())
+            return 1.0;
+
+        double msec = std::max(1, length->get_value());
+        return get_elapsed() / msec;
+    }
+};
+
+
+wf::animation::duration_t::duration_t(
+    std::shared_ptr<wf::config::option_t<int>> length,
+    smoothing::smooth_function smooth)
+{
+    this->priv = std::make_unique<impl> ();
+    this->priv->length = length;
+    this->priv->is_running = false;
+    this->priv->smooth_function = smooth;
+}
+
+wf::animation::duration_t::~duration_t() = default;
+
+/**
+ * Start the duration.
+ * This means that the progress will get reset to 0.
+ */
+void wf::animation::duration_t::start()
+{
+    this->priv->is_running = 1;
+    this->priv->start_point = std::chrono::system_clock::now();
+}
+
+double wf::animation::duration_t::progress() const
+{
+    if (this->priv->is_ready())
+        return 1.0;
+
+    return this->priv->smooth_function(
+        this->priv->get_progress_percentage());
+}
+
+bool wf::animation::duration_t::running()
+{
+    if (this->priv->is_ready())
+    {
+        bool was_running = this->priv->is_running;
+        this->priv->is_running = false;
+        return was_running;
+    }
+
+    return true;
+}
---
+++ test/duration_test.cpp
@@ -0,0 +1,46 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include <wayfire/config/option.hpp>
+#include <wayfire/util/duration.hpp>
+#include <unistd.h>
+
+using namespace wf;
+using namespace wf::config;
+using namespace wf::animation;
+
+TEST_CASE("wf::animation::duration_t")
+{
+    auto length = std::make_shared<option_t<int>>("length", 100);
+    duration_t duration{length, smoothing::linear};
+
+    auto check_lifetime = [&] ()
+    {
+        CHECK(duration.running() == false);
+        CHECK(duration.progress() == doctest::Approx{1.0});
+
+        duration.start();
+        CHECK(duration.running());
+        CHECK(duration.progress() == doctest::Approx{0.0});
+
+        usleep(50000);
+        CHECK(duration.progress() == doctest::Approx{0.5}.epsilon(0.1));
+        CHECK(duration.running());
+        CHECK(duration.running());
+        usleep(60000);
+
+        /* At this point, duration must be finished */
+        CHECK(duration.progress() == doctest::Approx{1.0}.epsilon(0.01));
+        CHECK(duration.running()); // one last time
+        CHECK(duration.running() == false);
+        CHECK(duration.running() == false);
+    };
+
+    /* Check twice, so that we can test restarting */
+    check_lifetime();
+    check_lifetime();
+}
+
+TEST_CASE("wf::animation::timed_transition_t")
+{
+}
