db41cf4192421bf0e907d4a617e67e2281e5fd31
Ilia Bozhinov <ammen99@gmail.com>
2020-12-27 10:01:36 +0100

    option-wrapper: add template specializations for compound options

--- test/option_wrapper_test.cpp
+++ test/option_wrapper_test.cpp
@@ -33,12 +33,27 @@ TEST_CASE("wf::base_option_wrapper_t")
 
     auto section = std::make_shared<section_t>("Test");
     auto opt     = std::make_shared<option_t<int>>("Option1", 5);
+
+    compound_option_t::entries_t entries;
+    entries.push_back(std::make_unique<compound_option_entry_t<int>>("hey_"));
+    entries.push_back(std::make_unique<compound_option_entry_t<double>>("bey_"));
+
+    auto coptr = new compound_option_t{"Option2", std::move(entries)};
+    auto copt  = std::shared_ptr<option_base_t>(coptr);
+
     section->register_new_option(opt);
+    section->register_new_option(copt);
     ::config.merge_section(section);
 
     wrapper_t<int> wrapper{"Test/Option1"};
     CHECK((option_sptr_t<int>)wrapper == opt);
     CHECK(wrapper == 5);
+
+    wrapper_t<compound_list_t<int, double>> wrapper2{"Test/Option2"};
+    CHECK((std::shared_ptr<compound_option_t>)wrapper2 == copt);
+    bool value_in_compound_list_is_ok =
+        (wrapper2.value() == compound_list_t<int, double>{});
+    CHECK(value_in_compound_list_is_ok);
 
     bool updated = false;
     wrapper.set_callback([&] ()
