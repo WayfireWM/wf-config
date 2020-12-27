a9d06780832bd63fbfd4f6c05ed7fda2c70024e2
Ilia Bozhinov <ammen99@gmail.com>
2020-12-27 10:01:36 +0100

    option-wrapper: add template specializations for compound options

--- include/wayfire/config/option-wrapper.hpp
+++ include/wayfire/config/option-wrapper.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <stdexcept>
 #include <wayfire/config/option.hpp>
+#include <wayfire/config/compound-option.hpp>
 
 namespace wf
 {
@@ -28,6 +30,22 @@ option_sptr_t<T> create_option_string(const std::string& value)
 }
 
 /**
+ * A helper to check whether the given type is a specialization of std::vector
+ */
+template<class>
+struct is_std_vector : public std::false_type {};
+
+template<class V, class A>
+struct is_std_vector<std::vector<V, A>> : public std::true_type {};
+
+template<class... Args>
+void get_value_from_compound_option(
+    config::compound_option_t *opt, config::compound_list_t<Args...>& list)
+{
+    list = opt->get_value<Args...>();
+}
+
+/**
  * A simple wrapper around a config option.
  *
  * This is a base class. Each application which uses it needs to subclass it
@@ -36,6 +54,12 @@ option_sptr_t<T> create_option_string(const std::string& value)
 template<class Type>
 class base_option_wrapper_t
 {
+  public:
+    using OptionType = std::conditional_t<
+        is_std_vector<Type>::value,
+        config::compound_option_t,
+        config::option_t<Type>>;
+
   public:
     base_option_wrapper_t(const base_option_wrapper_t& other) = delete;
     base_option_wrapper_t& operator =(
@@ -65,8 +89,7 @@ class base_option_wrapper_t
             throw std::runtime_error("No such option: " + std::string(name));
         }
 
-        raw_option = std::dynamic_pointer_cast<
-            wf::config::option_t<Type>>(untyped_option);
+        raw_option = std::dynamic_pointer_cast<OptionType>(untyped_option);
         if (raw_option == nullptr)
         {
             throw std::runtime_error("Bad option type: " + std::string(name));
@@ -86,10 +109,23 @@ class base_option_wrapper_t
     /** Implicitly convertible to the value of the option */
     operator Type() const
     {
-        return raw_option->get_value();
+        return this->value();
     }
 
-    operator option_sptr_t<Type>() const
+    Type value() const
+    {
+        if constexpr (is_std_vector<Type>::value)
+        {
+            Type list;
+            get_value_from_compound_option(this->raw_option.get(), list);
+            return list;
+        } else
+        {
+            return raw_option->get_value();
+        }
+    }
+
+    operator std::shared_ptr<OptionType>() const
     {
         return raw_option;
     }
@@ -105,7 +141,7 @@ class base_option_wrapper_t
     wf::config::option_base_t::updated_callback_t option_update_listener;
 
     /** The actual option wrapped by the option wrapper */
-    option_sptr_t<Type> raw_option;
+    std::shared_ptr<OptionType> raw_option;
 
     /**
      * Initialize the option wrapper.
