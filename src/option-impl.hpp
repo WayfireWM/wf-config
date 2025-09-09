5ae61ab9d333a9368f1d50db794e4b1ee8bfcb67
Ilia Bozhinov <ammen99@gmail.com>
2025-09-09 15:00:35 +0200

    ini-parser: emit errors for options not part of the XML spec (#83)

--- src/option-impl.hpp
+++ src/option-impl.hpp
@@ -6,21 +6,6 @@
 #include <libxml/tree.h>
 #include <stdint.h>
 
-struct wf::config::option_base_t::impl
-{
-    std::string name;
-    wf::safe_list_t<updated_callback_t*> updated_handlers;
-
-    // Number of times the option has been locked
-    int32_t lock_count = 0;
-
-    // Associated XML node
-    xmlNode *xml;
-
-    // Is option in config file?
-    bool option_in_config_file = false;
-};
-
 namespace wf
 {
 namespace config
@@ -37,3 +22,23 @@ void update_compound_from_section(compound_option_t& option,
     const std::shared_ptr<section_t>& section);
 }
 }
+
+struct wf::config::option_base_t::impl
+{
+    std::string name;
+    wf::safe_list_t<updated_callback_t*> updated_handlers;
+
+    // Number of times the option has been locked
+    int32_t lock_count = 0;
+
+    // Associated XML node
+    xmlNode *xml = nullptr;
+
+    // Is option in config file?
+    bool option_in_config_file = false;
+
+    // Is option part of a successfully parsed compound option?
+    bool is_part_compound = false;
+    // Does this option match a compound option in part at least?
+    bool could_be_compound = false;
+};
